b515cd48d91876dede61b868d5ef0477440c0603
jk-truong <41808114+jk-truong@users.noreply.github.com>
2020-09-17 23:37:04 -0700

    Ch 11 fin

---
+++ app/src/main/java/com/example/criminalintent/database/CrimeTypeConverters.kt
@@ -0,0 +1,31 @@
+package com.example.criminalintent.database
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class CrimeTypeConverters{
+
+    //Tells room how to convert the type for date
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(millisSinceEpoch: Long?): Date? {
+        return millisSinceEpoch?.let {
+            Date(it)
+        }
+    }
+
+    //for uuid
+    @TypeConverter
+    fun toUUID(uuid: String?): UUID? {
+        return UUID.fromString(uuid)
+    }
+
+    @TypeConverter
+    fun fromUUID(uuid: UUID?): String? {
+        return uuid?.toString()
+    }
+}
